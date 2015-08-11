c0cea864346a7cc3cf43d83b62a992b04a825044
Sam Green <samsfgreen@gmail.com>
2015-08-11 18:24:19 +0100

    restructuring folders

--- tests/testing_32Float_save.js
+++ tests/testing_32Float_save.js
@@ -6,7 +6,6 @@ var data = new Float32Array([1.1,2.2,3.3,4.4,5.5]);
 //prepare the length of the buffer to 4 bytes per float
 var buffer = new Buffer(data.length*4);
 
-
 for(var i = 0; i < data.length; i++){
     //write the float in Little-Endian and move the offset
     buffer.writeFloatLE(data[i], i*4);
